3225f74eb8c4abb8ec3ad81ca00cb1f52c0330bb
Tatyana Sugako <tatyanasugako@gmail.com>
2024-08-09 16:16:32 +0300

    feat: header layout done
    responsive header made (added burger menu on smaller screens) + implemented custom hook for burger menu

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,38 @@
+import './Header.scss';
+import museum_logo from '@assets/images/museum-logo.svg';
+import bookmark from '@assets/images/bookmark.svg';
+import { useBurgerMenu } from './useBurgerMenu';
+
+export default function Header() {
+  const [isOpen, isOverlay, toggle] = useBurgerMenu();
+
+  return (
+    <>
+      <ul className={`header__burger-list ${isOverlay ? '' : 'overlay'}`}>
+        <li className="header__item header__item--bigger">
+          <img src={bookmark} alt="Bookmark" className="header__icon header__icon--bigger" />
+          <a href="" className="header__link">
+            Your favorites
+          </a>
+        </li>
+      </ul>
+
+      <header className="header">
+        <img src={museum_logo} alt="Museum logo" className="header__logo" />
+
+        <div className={`header__burger ${isOpen ? '' : 'close'}`} onClick={toggle}>
+          <span></span>
+        </div>
+
+        <ul className="header__list">
+          <li className="header__item">
+            <img src={bookmark} alt="Bookmark" className="header__icon" />
+            <a href="" className="header__link">
+              Your favorites
+            </a>
+          </li>
+        </ul>
+      </header>
+    </>
+  );
+}
