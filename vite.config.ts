528567f0d9205f018011bcc2cbdd6c65c4debe0a
Tatyana Sugako <tatyanasugako@gmail.com>
2024-08-12 00:33:13 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,18 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://thoosaa.github.io/Art-Museum',
+  base: '/Art-Museum/',
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
       '@assets': path.resolve(__dirname, './src/assets'),
       '@pages': path.resolve(__dirname, './src/pages'),
       '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
+  server: {
+    proxy: {
+      '/assets': 'https://thoosaa.github.io/Art-Museum',
     },
   },
 });
