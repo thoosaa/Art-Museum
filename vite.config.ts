94401d000042b22b10ff0a2215ae1d626a2d4593
Tatyana Sugako <tatyanasugako@gmail.com>
2024-08-11 23:14:01 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/art-museum',
+  base: '/Art-Museum',
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
