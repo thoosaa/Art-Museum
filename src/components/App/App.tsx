996aae6d4986a0e3af1e2945c32db8aefe0fefcb
Tatyana Sugako <tatyanasugako@gmail.com>
2024-08-16 10:40:47 +0300

    add: file App.tsx with all routes

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import ArtPiece from '@pages/ArtPiece/ArtPiece';
+import Favorites from '@pages/Favorites/Favorites';
+import Home from '@pages/Home/Home';
+import { Routes, Route } from 'react-router-dom';
+
+export function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/favorites" element={<Favorites />} />
+      <Route path="/art/:artId" element={<ArtPiece />} />
+    </Routes>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 //import App from './App';
 import '@assets/styles/index.scss';
-import { HashRouter, Route, Routes } from 'react-router-dom';
-import Home from '@pages/Home/Home';
-import Favorites from '@pages/Favorites/Favorites';
-import ArtPiece from '@pages/ArtPiece/ArtPiece';
+import { HashRouter } from 'react-router-dom';
 import { PaginationProvider } from '@context/PageContext';
+import { App } from '@components/App/App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <PaginationProvider>
       <HashRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/art/:artId" element={<ArtPiece />} />
-        </Routes>
+        <App />
       </HashRouter>
     </PaginationProvider>
   </StrictMode>,
