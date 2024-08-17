3ccfcf20d211f21ab7a3519c6b6bdf3d9a57726f
Tatyana Sugako <tatyanasugako@gmail.com>
2024-08-17 15:49:12 +0300

    add: custom hook for other gallery, handles fetching, loading and errors

--- src/components/OtherGalley/OtherGallery.tsx
+++ src/components/OtherGalley/OtherGallery.tsx
@@ -2,32 +2,14 @@ import './OtherGallery.scss';
 
 import ArtCardSmall from '@components/ArtCardSmall/ArtCardSmall';
 import Loader from '@components/Loader/Loader';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useArtworks } from '@hooks/useArtworks';
 
 export default function OtherGallery() {
-  const [art, setArt] = useState<string[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    fetchArt();
-  }, []);
-
-  const fetchArt = async () => {
-    try {
-      setIsLoading(true);
-      const res = await axios.get(`https://api.artic.edu/api/v1/artworks?limit=9`);
-      console.log(res.data.data.map((item: { id: number }) => item.id));
-      setArt(res.data.data.map((item: { id: number }) => item.id));
-      setIsLoading(false);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  const { art, isLoading, error } = useArtworks();
 
   return (
     <div className="other-gallery">
-      {isLoading ? <Loader /> : art.map((id) => <ArtCardSmall art_id={id} key={id} />)}
+      {error ? <h1>{error}</h1> : isLoading ? <Loader /> : art.map((id) => <ArtCardSmall art_id={id} key={id} />)}
     </div>
   );
 }
---
+++ src/hooks/useArtworks.ts
@@ -0,0 +1,30 @@
+import { BASE_URL } from '@constants/api_routes';
+import getErrorMessage from '@utils/helperFunctions/getErrorMessage';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+export function useArtworks() {
+  const [art, setArt] = useState<string[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>('');
+  const numberOfArtworks = 9;
+
+  useEffect(() => {
+    const fetchArt = async () => {
+      try {
+        setIsLoading(true);
+        const res = await axios.get(`${BASE_URL}?limit=${numberOfArtworks}`);
+        console.log(res.data.data.map((item: { id: number }) => item.id));
+        setArt(res.data.data.map((item: { id: number }) => item.id));
+        setIsLoading(false);
+      } catch (err) {
+        console.log(err);
+        setError(getErrorMessage(error));
+      }
+    };
+    fetchArt();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return { art, isLoading, error };
+}
